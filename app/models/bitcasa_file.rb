c821b466bed530a1250f845e86bdcd1078ca3089
pyar6329 <pyar6329@gmail.com>
2014-10-17 16:50:01 +0900

    #38 rm bitcasa_files table

--- app/models/bitcasa_file.rb
+++
@@ -1,8 +0,0 @@
-class BitcasaFile < ActiveRecord::Base
-  belongs_to :path
-  belongs_to :broadcast
-  belongs_to :size
-  belongs_to :extension
-  belongs_to :genre
-  belongs_to :bitcasa_time
-end
