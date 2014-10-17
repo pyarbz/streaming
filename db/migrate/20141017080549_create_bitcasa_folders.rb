5f09525f24dc63dc5a7ae416e113327c13a2173c
pyar6329 <pyar6329@gmail.com>
2014-10-17 17:06:17 +0900

    #38 rails g model bitcasa_folder name:string path:references genre:references bitcasa_time:references size:references

---
+++ db/migrate/20141017080549_create_bitcasa_folders.rb
@@ -0,0 +1,13 @@
+class CreateBitcasaFolders < ActiveRecord::Migration
+  def change
+    create_table :bitcasa_folders do |t|
+      t.string :name
+      t.references :path, index: true
+      t.references :genre, index: true
+      t.references :bitcasa_time, index: true
+      t.references :size, index: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/bitcasa_folders.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :bitcasa_folder do
+    name "MyString"
+    path nil
+    genre nil
+    bitcasa_time nil
+    size nil
+  end
+end
