367223dbc27a3a04c09129f954eea89366050338
pyar6329 <pyar6329@gmail.com>
2014-10-17 16:42:17 +0900

    #38 rails g model folder name:string path:references genre:references bitcasa_time:references size:references

---
+++ app/models/folder.rb
@@ -0,0 +1,6 @@
+class Folder < ActiveRecord::Base
+  belongs_to :path
+  belongs_to :genre
+  belongs_to :bitcasa_time
+  belongs_to :size
+end
---
+++ spec/factories/folders.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :folder do
+    name "MyString"
+    path nil
+    genre nil
+    bitcasa_time nil
+    size nil
+  end
+end
